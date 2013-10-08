f0d98dffa13ce528ab47690c29f9ec0137a81b72
Drup <drupyog@zoho.com>
2013-10-08 19:55:52 +0200

    Adapt Pretty printing to term changes.

--- PPrint.hs
+++ PPrint.hs
@@ -4,6 +4,7 @@ import Names
 import Terms
 
 import Text.PrettyPrint
+import Numeric (showIntAtBase)
 
 -- default indentation
 indentation :: Int
@@ -13,15 +14,19 @@ ident :: Ident -> Doc
 ident (i,n,_) = text n <> int i
 
 -- x:A
-vartype :: Ident -> Term -> Doc
-vartype x tyA = ident x <> colon <> term tyA
+vartype :: Ident -> Ident -> Doc
+vartype x tyA = ident x <> colon <> ident tyA
+
+-- x:*ᵢ
+varsort :: Ident -> Int -> Doc
+varsort x s = ident x <> colon <> sort s
 
 -- (x:A)×B
-sigmaP :: Ident -> Term -> Term -> Doc
+sigmaP :: Ident -> Ident -> Term -> Doc
 sigmaP x tyA tyB = parens (vartype x tyA) <> text "×" <> term tyB
 
 -- (x:A)→B
-piP :: Ident -> Term -> Term -> Doc
+piP :: Ident -> Ident -> Term -> Doc
 piP x tyA tyB = parens (vartype x tyA) <> text "→" <> term tyB
 
 -- { 'tag₁ , ... 'tagₙ }
@@ -33,9 +38,9 @@ pairP :: Ident -> Ident -> Doc
 pairP x y = parens (ident x <> text ", " <> ident y)
 
 -- λ(x:A).t
-lamP :: Ident -> Term -> Term -> Doc
-lamP x tyA t =
-    hang (char 'λ' <> vartype x tyA <> char '.')
+lamP :: Ident -> Term -> Doc
+lamP x t =
+    hang (char 'λ' <> ident x <> char '.')
          indentation (term t)
 
 -- case x do { 'tagᵢ → tᵢ | i = 1..n }
@@ -60,27 +65,41 @@ letP1 i = letP (ident i)
 letP2 :: Ident -> Ident -> Doc -> Doc -> Doc
 letP2 i1 i2 = letP $ pairP i1 i2
 
+star :: Doc
+star = text "★"
+
+scriptPretty :: String -> Int -> Doc
+scriptPretty s = text . scriptShow s
+
+scriptShow (minus:digits) x = if x < 0 then minus : sho (negate x) else sho x
+  where sho x = showIntAtBase 10 (\i -> digits !! i) x []
+
+subscriptPretty = scriptPretty "-₀₁₂₃₄₅₆₇₈₉"
+
+sort :: Int -> Doc
+sort s = star <> subscriptPretty s
 
 term :: Term -> Doc
 term (t,_) =
     case t of
       Var i -> ident i
 
-      Pi i x tyA tyB t' ->
-          letP1 i (piP x tyA tyB) (term t')
-      Lam i x tyA tfun t' ->
-          letP1 i (lamP x tyA tfun) (term t')
+      Pi i s x tyA tyB t' ->
+          letP (varsort i s) (piP x tyA tyB) (term t')
+      Lam i ty x tfun t' ->
+          letP (vartype i ty) (lamP x tfun) (term t')
       App i f x t' ->
           letP1 i (ident f <+> ident x) (term t')
 
-      Sigma i x tyA tyB t' ->
-          letP1 i (sigmaP x tyA tyB) (term t')
-      Pair i x y t' ->
-          letP1 i (pairP x y) (term t')
+      Sigma i s x tyA tyB t' ->
+          letP (varsort i s) (sigmaP x tyA tyB) (term t')
+      Pair i ty x y t' ->
+          letP (vartype i ty) (pairP x y) (term t')
       Proj i1 i2 z t' ->
           letP2 i1 i2 (ident z) (term t')
 
       Fin i l t' ->
           letP1 i (finP l) (term t')
-      Tag s -> text s
+      Tag i ty s t' -> letP (vartype i ty) (text s) (term t')
       Case x l -> caseP x l
+      Star i -> sort i
