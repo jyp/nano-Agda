256fdaaa47f85d2cfba5e9fb0ead24c36770f95b
Drup <drupyog@zoho.com>
2013-10-10 19:36:34 +0200

    Make the whole thing actually compile.

---
+++ Main.hs
@@ -0,0 +1,33 @@
+module Main where
+
+import qualified RawSyntax as R
+import qualified Lexer as L
+import qualified Parser as P
+import Common
+import Names
+import Terms
+import Typing
+import Env
+import System.Environment(getArgs)
+
+parseFile :: String -> Err [(Ident,Term,Term)]
+parseFile f =
+    let rawfile = P.main (L.alexScanTokens f) in
+    fmap R.convertFile rawfile
+
+parseFiles :: [String] -> Err [(Ident,Term,Term)]
+parseFiles l = do
+  l' <- mapM parseFile l
+  return (concat l')
+
+checkFiles :: [(Ident,Term,Type)] -> Err [(Env,Term)]
+checkFiles decs = do
+  decsT <- mapM (convert . checkDec) decs
+  return decsT
+
+main = do
+  args <- getArgs
+  files <- mapM readFile args
+  let decs = parseFiles files
+  let results = decs >>= checkFiles
+  return ()
