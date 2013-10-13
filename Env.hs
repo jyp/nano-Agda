12048b2bbb670d4d4af540d8e43cc8beb58cd406
Drup <drupyog@zoho.com>
2013-10-13 16:33:29 +0200

    Anotate Definitions with intro/eliom for tags and pairs, add new env utility functions.

--- Env.hs
+++ Env.hs
@@ -10,8 +10,10 @@ import qualified Data.Map as M
 data Definition
     = Appl Ident Ident          -- y = f x
     | Fun Ident T.Term          -- f = λx.t
-    | Tag String                -- x = 'tag
-    | Pair Ident Ident          -- z = (x,y)
+    | ITag String               -- x = 'tag
+    | ETag String               -- x = 'tag
+    | IPair Ident Ident         -- z = (x,y)
+    | EPair Ident Ident         -- (x,y) = z
     | Alias Ident               -- z = z'
     | Pi Ident Ident T.Term     -- σ = (x:A)→B
     | Sigma Ident Ident T.Term  -- σ = (x:A)×B
@@ -29,6 +31,22 @@ getSort (Env c _ _) i =
     case fst (c M.! i) of
       T.Star n -> n
 
+getType :: Env -> Ident -> T.Type
+getType (Env c _ _) i = c M.! i
+
+getIntro :: Env -> Ident -> Definition
+getIntro (Env _ e _) i = e M.! i
+
+getElims :: Env -> Ident -> [Definition]
+getElims (Env _ _ e) i = e M.! i
+
+getVal :: Env -> Ident -> Either Definition [Definition]
+getVal e@(Env _ ei ee) i =
+    case M.lookup i ei of
+      Just (Alias x) -> getVal e x
+      Just x -> Left x
+      Nothing -> Right $ ee M.! i
+
 addContext :: Env -> Ident -> T.Type -> TypeError Env
 addContext (Env context ei ee) i ty = do
     context' <-
@@ -37,6 +55,16 @@ addContext (Env context ei ee) i ty = do
           Just ty' | ty == ty' -> return (M.insert i ty context)
           Just ty' -> throw (IncompBindings i ty ty')
     return (Env context' ei ee)
+
+addBinding :: Env -> Ident -> Definition -> T.Type -> TypeError Env
+addBinding env i t ty = do
+  (Env context ei ee) <- addContext env i ty
+  let ei' = M.insert i t ei
+  return (Env context ei' ee)
+
+addAlias :: Env -> Ident -> Ident -> TypeError Env
+addAlias env x y =
+    addBinding env x (Alias y) (getType env y)
 
 -- Intro Type in the environment
 
@@ -58,3 +86,10 @@ typeFin env i ty l = do
   envi' <- return (M.insert i (Fin l) envi)
   return (Env context envi' enve)
 
+elimPair :: Env -> Ident -> Ident -> Ident -> TypeError Env
+elimPair env@(Env c ei ee) x' y' z =
+    case M.lookup z ei of
+      Just (IPair x y) ->
+          do { env' <- addAlias env x' x ; addAlias env' y' y }
+      Nothing ->
+          return $ Env c (M.insert z (EPair x' y') ei) ee
