4c87a34bd43d9899c5fd21341e26e5dbcd4239fa
Drup <drupyog@zoho.com>
2013-10-14 16:16:58 +0200

    Make the top level better.

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified RawSyntax as R
 import qualified Lexer as L
 import qualified Parser as P
+import qualified PPrint as PPrint
 import Common
 import Names
 import Terms
@@ -11,14 +12,9 @@ import Env
 import System.Environment(getArgs)
 import Control.Monad.Error
 
-parseFile :: String -> Err [(Ident,Term,Term)]
-parseFile f = do
-  rawfile <- P.main (L.alexScanTokens f)
-  R.convertFile rawfile
-
-parseFiles :: [String] -> Err [(Ident,Term,Term)]
+parseFiles :: [String] -> Err [R.Smt]
 parseFiles l = do
-  l' <- mapM parseFile l
+  l' <- mapM (P.main . L.alexScanTokens) l
   return (concat l')
 
 checkFiles :: [(Ident,Term,Type)] -> Err [(Env, Term, Type)]
@@ -26,12 +22,26 @@ checkFiles decs = do
   decsT <- mapM (convert . checkDec) decs
   return decsT
 
+printExn :: Show a => ErrorT e IO a -> IO ()
+printExn e = do
+    e' <- runErrorT e
+    case e' of
+      Left _ -> return ()
+      Right x -> print x
+
 main :: IO ()
 main = do
   args <- getArgs
   files <- mapM readFile args
-  let decs = parseFiles files
-  results <- runErrorT ( decs >>= checkFiles )
+  -- () <- print files
+
+  let decsAST = parseFiles files
+  () <- printExn decsAST
+
+  let decsTerms = decsAST >>= R.convertFile
+  () <- printExn (fmap PPrint.smts decsTerms)
+
+  results <- runErrorT $ decsTerms >>= checkFiles
   case results of
     Left x -> print x
     Right x -> print x
