48c71fc7e2aedb7d78d4ed477b11e47cd8c75fdb
Drup <drupyog@zoho.com>
2013-12-06 01:05:46 +0100

    Fix issues with reduction in normalisation in case of variable replacement.

--- NormalForm.hs
+++ NormalForm.hs
@@ -57,6 +57,10 @@ subs' :: NF -> Ident -> Con -> NF
 subs' (Case x l) i c@(Tag tag) | i == x =
   let branch = Maybe.fromJust $ List.lookup tag l in
   subs' branch i c
+subs' (Case x l) i s@(Var x') | i == x =
+  Case x' (map f l)
+       where
+         f (tag,n) = (tag, subs' n i s)
 subs' (Case x l) i s =
   Case x (map f l)
        where
@@ -67,6 +71,8 @@ subs' (App y f x n) i c@(Lam a nf) | i == f =
       nf_y = subs' nf a x'
       n'   = subs  n  y nf_y in
   subs' n' f c
+subs' (App y _f x n) i c@(Var f') =
+    App y f' (subsC x i c) (subs' n i c)
 subs' (App y f x n) i c =
     App y f (subsC x i c) (subs' n i c)
 
@@ -74,6 +80,8 @@ subs' (Proj x y z n) i c@(Pair x' y') | i == z =
   let n_x = subs' n   x x'
       n_y = subs' n_x y y' in
   subs' n_y i c
+subs' (Proj x y _z n) i c@(Var z') =
+    Proj x y z' (subs' n i c)
 subs' (Proj x y z n) i c =
     Proj x y z (subs' n i c)
 subs' (Con c) i s = Con (subsC c i s)
